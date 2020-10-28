5b938ef12d8da3942dbf7c2a28082d684afa9ee9
Justin Yarbrough <justin.yarbrough@twisted-rope.com>
2020-10-28 07:55:20 -0400

    Update env variables to Next v10 method

--- frontend/next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  env: {
-    HDP_APP_URI: 'http://localhost:3000',
-    HDP_ENDPOINT: 'http://localhost:7777',
-  },
-};
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,12 @@
 const Home = props => {
-  return <h1>Hello, world!</h1>;
+  return (
+    <div>
+      <h1>Hello, world</h1>
+
+      <h5>Env: {process.env.NODE_ENV}</h5>
+      <h5>App URI: {process.env.NEXT_PUBLIC_APP_URI}</h5>
+    </div>
+  );
 };
 
 export default Home;
